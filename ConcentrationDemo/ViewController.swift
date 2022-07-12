727e8ec859ec7f20990e0fb79092c9e072a68fac
Artem Paliutin <pal.artem@icloud.com>
2022-07-12 20:05:47 +0300

    Initial Commit

---
+++ ConcentrationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConcentrationDemo
+//
+//  Created by Artem Paliutin on 12/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
